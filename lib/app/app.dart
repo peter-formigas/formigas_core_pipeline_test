31c5197fcc0e3dfc14f12cccccc9cbf72832e183
Peter Trost <peter@formigas.de>
2024-02-09 12:18:23 +0100

    fix: Organize imports

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:formigas_core_pipeline_test/common/custom_themes.dart';
 import 'package:formigas_core_pipeline_test/feature/counter/counter_controller.dart';
 import 'package:formigas_core_pipeline_test/feature/counter/counter_view.dart';
-import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
